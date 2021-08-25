d456efef2286435096f051c24ba708180bf8383c
MajkiITStudent <michal.pierzchala99@poczta.fm>
2021-08-25 18:37:58 +0200

    event form controller added

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Form\UploadEventType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +14,28 @@ class UploadController extends AbstractController
     /**
      * @Route("/add", name="add_event")
      */
-    public function upload(): Response
+    public function upload(Request $request): Response
     {
         $form  = $this->createForm(UploadEventType::class);
+
+        $form -> handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+
+            if($this->getUser()){
+                $entityEvent = new Event();
+                $entityEvent->setUser($this->getUser());
+                $entityEvent->setFilename($form->get('filename')->getData());
+                $entityEvent->setUploadedAt(new \DateTime());
+                $entityEvent->setDescription($form->get('description')->getData());
+                $entityEvent->setSportType($form->get('sport_type')->getData());
+                $entityEvent->setEventDatetime($form->get('event_datetime')->getData());
+                $entityEvent->setPeopleNeeded($form->get('people_needed')->getData());
+
+                $entityManager -> persist($entityEvent);
+                $entityManager -> flush();
+            }
+        }
 
         return $this->render('upload/upload.html.twig', [
             'uploadForm' => $form->createView(),
--- src/Form/UploadEventType.php
+++ src/Form/UploadEventType.php
@@ -4,15 +4,28 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UploadEventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('filename')
+            ->add('filename', FileType::class,[
+                'label' => '',
+                'constraints' =>[
+                    new File([
+                        'maxSize' => '4M',
+                        'mimeTypes' => [
+                            'image/*'
+                        ],
+                        'mimeTypesMessage' => 'Error, not supported image file'
+                    ])
+                ]
+            ])
             ->add('description')
             ->add('sport_type')
             ->add('event_datetime')
