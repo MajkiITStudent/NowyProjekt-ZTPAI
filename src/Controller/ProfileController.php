2702e920e695691ad6f241d2893a3bdd493010bb
MajkiITStudent <michal.pierzchala99@poczta.fm>
2021-08-26 19:07:36 +0200

    fixed profile controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -13,43 +13,67 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile/{id}", name="profile")
-     * @param int $id
+     * @Route("/profile", name="profile")
+     * @param Request $request
      * @return Response
      */
-    public function index(int $id, Request $request): Response
+    public function index(Request $request): Response
     {
         $form  = $this->createForm(UpdateProfileType::class);
         $form -> handleRequest($request);
 
+        $ajDi = $this->getUser()->getId();
+        $entityManager = $this->getDoctrine()->getManager();
+        $update = $entityManager->getRepository(UserDetails::class)->findOneBy(['user' => $ajDi]);
+
         if($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-
             if($this->getUser()){
-                try{
-                    $entityUserDetails = new UserDetails();
-                    $entityUserDetails->setUser($this->getUser());
-                    $entityUserDetails->setName($form->get('name')->getData());
-                    $entityUserDetails->setSurname($form->get('surname')->getData());
-                    $entityUserDetails->setCountry($form->get('country')->getData());
-                    $entityUserDetails->setCity($form->get('city')->getData());
-                    $entityUserDetails->setAge($form->get('age')->getData());
-                    $entityUserDetails->setFavouriteSport($form->get('favourite_sport')->getData());
+                if($update instanceof UserDetails){
+                    try{
+                        $update->setName($form->get('name')->getData());
+                        $update->setSurname($form->get('surname')->getData());
+                        $update->setCountry($form->get('country')->getData());
+                        $update->setCity($form->get('city')->getData());
+                        $update->setAge($form->get('age')->getData());
+                        $update->setFavouriteSport($form->get('favourite_sport')->getData());
 
-                    $entityManager -> persist($entityUserDetails);
-                    $entityManager -> flush();
+                        $entityManager -> persist($update);
+                        $entityManager -> flush();
 
-                    $this->addFlash('success', 'You succesfully updated your user details data');
-                }catch (\Exception $e){
-                    $this->addFlash('error', 'An error occured while trying to update your data');
+                        $this->addFlash('success', 'You succesfully updated your user details data');
+                    }catch (\Exception $e){
+                        $this->addFlash('error', 'An error occured while trying to update your data');
+                    }
                 }
-            }else{
+                else{
+                    try{
+                        $entityUserDetails = new UserDetails();
+                        $entityUserDetails->setUser($this->getUser());
+                        $entityUserDetails->setName($form->get('name')->getData());
+                        $entityUserDetails->setSurname($form->get('surname')->getData());
+                        $entityUserDetails->setCountry($form->get('country')->getData());
+                        $entityUserDetails->setCity($form->get('city')->getData());
+                        $entityUserDetails->setAge($form->get('age')->getData());
+                        $entityUserDetails->setFavouriteSport($form->get('favourite_sport')->getData());
+                        $entityManager -> persist($entityUserDetails);
+                        $entityManager -> flush();
+
+                        $this->addFlash('success', 'You succesfully filled your user details ');
+                    }catch (\Exception $e){
+                        $this->addFlash('error', 'An error occured while trying to fill your data for first time');
+                    }
+                }
+            }
+            else{
                 $this->addFlash('error', 'You are not logged in or its not tour account');
             }
         }
 
+        $allDetails = $entityManager->getRepository(UserDetails::class)->findAll();
+
         return $this->render('profile/profile.html.twig', [
             'profileForm' => $form->createView(),
+            'allDetails' => $allDetails
         ]);
     }
 }
