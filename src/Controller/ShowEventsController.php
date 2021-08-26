308ded92e5aa1e50c2ae0642051134d17b4ee4a2
MajkiITStudent <michal.pierzchala99@poczta.fm>
2021-08-26 14:09:05 +0200

    remove button works

--- src/Controller/ShowEventsController.php
+++ src/Controller/ShowEventsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Event;
 use PHPUnit\Util\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -45,9 +46,38 @@ class ShowEventsController extends AbstractController
                 $this->addFlash('error','Something go wrong :(');
             }
         }else{
-            $this->addFlash('error','You mus be logged in to do perform action');
+            $this->addFlash('error','You must be logged in to do perform action');
         }
 
+        return $this->redirectToRoute("show_events");
+    }
+
+    /**
+     * @Route("/show/remove/{id}", name="remove_event")
+     * @param int $id
+     * @return RedirectResponse
+     */
+    public function removeEvent(int $id){
+        $entityManager = $this->getDoctrine()->getManager();
+        $eventToRemove = $entityManager->getRepository(Event::class)->find($id);
+
+        if($this->getUser() && $this->getUser() == $eventToRemove->getUser()){
+            try{
+                $fileToRemove = new Filesystem();
+                $fileToRemove->remove('assets/images/events/'.$eventToRemove->getFilename());
+                if($fileToRemove->exists('assets/images/events/'.$eventToRemove->getFilename())){
+                    $this->addFlash('error','We could not manage your order to remove this event');
+                }else{
+                    $entityManager->remove($eventToRemove);
+                    $entityManager->flush();
+                    $this->addFlash('success','You succesfully deleted your event');
+                }
+            }catch (Exception $e){
+                $this->addFlash('error','Something go wrong :(');
+            }
+        }else{
+            $this->addFlash('error','You are logged out or you are not owner of this event');
+        }
         return $this->redirectToRoute("show_events");
     }
 }
