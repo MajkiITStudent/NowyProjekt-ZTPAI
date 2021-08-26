12a040df3612593e4cc23c82e4cdef734084a10a
MajkiITStudent <michal.pierzchala99@poczta.fm>
2021-08-26 13:50:30 +0200

    attend to event possible

--- src/Controller/ShowEventsController.php
+++ src/Controller/ShowEventsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use PHPUnit\Util\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +14,40 @@ class ShowEventsController extends AbstractController
     /**
      * @Route("/show", name="show_events")
      */
-    public function index(): Response
+    public function showEvents(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $allEvents = $entityManager->getRepository(Event::class)->findAll();
 
-        return $this->render('show_events/index.html.twig', [
+        return $this->render('show_events/showEvents.html.twig', [
             'allEvents' => $allEvents
         ]);
+    }
+
+    /**
+     * @Route("/show/take_part/{id}", name="take_part")
+     * @param int $id
+     * @return RedirectResponse
+     */
+    public function takePart(int $id): RedirectResponse
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $takePartAtEvent = $entityManager->getRepository(Event::class)->find($id);
+        $howMany = $takePartAtEvent->getPeopleNeeded();
+
+        if($this->getUser()){
+            try{
+                $takePartAtEvent->setPeopleNeeded($howMany -1);
+                $entityManager->persist($takePartAtEvent);
+                $entityManager->flush();
+                $this->addFlash('success','Success, you will atend this event!');
+            }catch(Exception $e){
+                $this->addFlash('error','Something go wrong :(');
+            }
+        }else{
+            $this->addFlash('error','You mus be logged in to do perform action');
+        }
+
+        return $this->redirectToRoute("show_events");
     }
 }
